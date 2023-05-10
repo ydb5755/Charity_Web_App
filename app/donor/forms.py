22eaa20964875edea8e53e80ab7b6d5b644ef2f0
yisroelBaum <yisroel.d.baum@gmail.com>
2023-05-10 15:18:31 +0300

    admin pages and routes

--- app/donor/forms.py
+++ app/donor/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Donor
 
 class AddAdmin(FlaskForm):
-    id = StringField('User ID', validators=[DataRequired()])
+    id = IntegerField('User ID', validators=[DataRequired()])
     submit = SubmitField('Add Admin')
+
+    def validate_id(form, field):
+        donor = Donor.query.filter_by(id=field.data).first()
+        if not donor:
+            raise ValidationError('This user does not exist, try another id')
+        if donor.admin == True:
+            raise ValidationError('This user is already an admin')
+        
 
 class RemoveAdmin(FlaskForm):
     admin = SelectField('Select Admin to remove priviliges', validators=[DataRequired()])
--- app/donor/routes.py
+++ app/donor/routes.py
@@ -6,28 +6,13 @@ from app.donor.forms import AddAdmin, RemoveAdmin, AddFunds
 from app import db
 
 
-@donor.route('/profile/<donor_id>')
+@donor.route('/profile/<donor_id>', methods=('GET', 'POST'))
 @login_required
 def profile_page(donor_id):
     if not int(current_user.id) == int(donor_id):
         return redirect(url_for('main.home'))
     donor = Donor.query.filter_by(id=donor_id).first()
     charities_to_be_confirmed = Charity.query.filter_by(authenticated=False)
-    add_admin_form = AddAdmin()
-    if add_admin_form.validate_on_submit():
-        user = Donor.query.filter_by(id=add_admin_form.id.data).first()
-        user.admin = True
-        db.session.commit()
-        flash('Admin priviliges added to user')
-        return redirect(url_for('donor.profile_page', donor_id=current_user.id))
-    remove_admin_form = RemoveAdmin()
-    remove_admin_form.choices = [(user.email) for user in Donor.query.filter_by(admin=True)]  #.remove(Donor.query.filter_by(id=1).first())
-    if remove_admin_form.validate_on_submit():
-        user = Donor.query.filter_by(email=remove_admin_form.admin.data)
-        user.admin = False
-        db.session.commit()
-        flash('Admin priviliges removed from user')
-        return redirect(url_for('donor.profile_page', donor_id=current_user.id))
     add_funds_form = AddFunds()
     if add_funds_form.validate_on_submit():
         donor.current_balance += add_funds_form.amount.data
@@ -37,7 +22,38 @@ def profile_page(donor_id):
     return render_template('profile.html',
                            donor=donor,
                            charities_to_be_confirmed=charities_to_be_confirmed,
-                           add_admin_form=add_admin_form,
-                           remove_admin_form=remove_admin_form,
                            add_funds_form=add_funds_form)
 
+@donor.route('/add_admin', methods=('GET', 'POST'))
+@login_required
+def add_admin():
+    if not current_user.id == 1:
+        return redirect(url_for('main.home'))
+    add_admin_form = AddAdmin()
+    if add_admin_form.validate_on_submit():
+        user = Donor.query.filter_by(id=add_admin_form.id.data).first()
+        user.admin = True
+        db.session.commit() 
+        flash('Admin priviliges added to user')
+        return redirect(url_for('donor.profile_page', donor_id=current_user.id))
+    return render_template('add_admin.html',
+                           add_admin_form=add_admin_form)
+
+
+@donor.route('/remove_admin', methods=('GET', 'POST'))
+@login_required
+def remove_admin():
+    if not current_user.id == 1:
+        return redirect(url_for('main.home'))
+    remove_admin_form = RemoveAdmin()
+    admins = Donor.query.filter_by(admin=True).all()
+    print(admins)
+    remove_admin_form.admin.choices = [user.email for user in admins]  #.remove(Donor.query.filter_by(id=1).first())
+    if remove_admin_form.validate_on_submit():
+        user = Donor.query.filter_by(email=remove_admin_form.admin.data).first()
+        user.admin = False
+        db.session.commit()
+        flash('Admin priviliges removed from user')
+        return redirect(url_for('donor.profile_page', donor_id=current_user.id))
+    return render_template('remove_admin.html',
+                           remove_admin_form=remove_admin_form)
